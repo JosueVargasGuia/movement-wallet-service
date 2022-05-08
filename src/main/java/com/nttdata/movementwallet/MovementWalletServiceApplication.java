2482ac5bff25e087a510ebf84496b125c5d082f0
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-05-07 23:20:27 -0500

    start

---
+++ src/main/java/com/nttdata/movementwallet/MovementWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.movementwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/movementwallet/MovementWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.movementwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
