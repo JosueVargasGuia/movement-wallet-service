25ab749e25c2eb6b747ed6a28a9d384ea3e37f08
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-05-11 11:55:09 -0500

    Reactive Redis

--- src/main/java/com/nttdata/movementwallet/config/RedisConfig.java
+++ src/main/java/com/nttdata/movementwallet/config/RedisConfig.java
@@ -1,27 +1,28 @@
 package com.nttdata.movementwallet.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.nttdata.movementwallet.entity.MovementWallet;
+
 @Configuration
-@ConditionalOnProperty(name = "cache.enabled", havingValue = "true")
 public class RedisConfig {
 	@Bean
-	public ReactiveHashOperations<String, Integer, MovementWallet> hashOperations(
-			ReactiveRedisConnectionFactory redisConnectionFactory) {
-		var template = new ReactiveRedisTemplate<>(redisConnectionFactory,
-				RedisSerializationContext.<String, MovementWallet>newSerializationContext(new StringRedisSerializer())
-						.hashKey(new GenericToStringSerializer<>(Integer.class))
-						.hashValue(new Jackson2JsonRedisSerializer<>(MovementWallet.class)).build());
-		return template.opsForHash();
+	ReactiveRedisOperations<String, MovementWallet> redisOperations(ReactiveRedisConnectionFactory factory) {
+		Jackson2JsonRedisSerializer<MovementWallet> serializer = new Jackson2JsonRedisSerializer<>(
+				MovementWallet.class);
+
+		RedisSerializationContext.RedisSerializationContextBuilder<String, MovementWallet> builder = RedisSerializationContext
+				.newSerializationContext(new StringRedisSerializer());
+
+		RedisSerializationContext<String, MovementWallet> context = builder.value(serializer).build();
+
+		return new ReactiveRedisTemplate<>(factory, context);
 	}
 }
--- src/main/java/com/nttdata/movementwallet/service/impl/MovementWalletServiceImpl.java
+++ src/main/java/com/nttdata/movementwallet/service/impl/MovementWalletServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -19,17 +20,16 @@ import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Log4j2 
+@Log4j2
 @Service
- //@ConditionalOnProperty(name = "cache.enabled", havingValue = "true")
+// @ConditionalOnProperty(name = "cache.enabled", havingValue = "true")
 public class MovementWalletServiceImpl implements MovementWalletService {
 
 	@Autowired
 	MovementWalletRepository movementWalletRepository;
-	//@Autowired
-    //private RedisTemplate<K, V> redisTemplate;
-	//  @Autowired
-	//private ReactiveHashOperations<String, Integer, MovementWallet> hashOperations;
+	@Autowired
+	ReactiveRedisOperations<String, MovementWallet> redisOperations;
+
 	@Override
 	public Flux<MovementWallet> findAll() {
 		// TODO Auto-generated method stub
@@ -40,7 +40,16 @@ public class MovementWalletServiceImpl implements MovementWalletService {
 	@Override
 	public Mono<MovementWallet> findById(Long idMovementWallet) {
 		// TODO Auto-generated method stub
-		return movementWalletRepository.findById(idMovementWallet);
+
+		return redisOperations.opsForValue().get(MovementWallet.class.getName() + idMovementWallet).map(e -> {
+			log.info("redis read:" + e.toString());
+			return e;
+		}).switchIfEmpty(movementWalletRepository.findById(idMovementWallet).map(e -> {
+			log.info("redis save:" + e.toString());
+			redisOperations.opsForValue().set(MovementWallet.class.getName() + e.getIdMovementWallet().toString(), e).subscribe();
+			return e;
+		}));
+		// return movementWalletRepository.findById(idMovementWallet);
 	}
 
 	@Override
@@ -63,13 +72,23 @@ public class MovementWalletServiceImpl implements MovementWalletService {
 		}
 		movementWallet.setIdMovementWallet(idMovementWallet + 1);
 		movementWallet.setDateModified(Calendar.getInstance().getTime());
-		return movementWalletRepository.insert(movementWallet);
+
+		return movementWalletRepository.insert(movementWallet).map(e -> {
+			log.info("redis save:" + e.toString());
+			redisOperations.opsForValue().set(MovementWallet.class.getName() + e.getIdMovementWallet().toString(), e).subscribe();
+			return e;
+		});
 	}
 
 	@Override
 	public Mono<MovementWallet> update(MovementWallet movementWallet) {
 		// TODO Auto-generated method stub
-		return movementWalletRepository.save(movementWallet);
+
+		return movementWalletRepository.save(movementWallet).map(e -> {
+			log.info("redis update:" + e.toString());
+			redisOperations.opsForValue().set(MovementWallet.class.getName() + e.getIdMovementWallet().toString(), e).subscribe();
+			return e;
+		});
 	}
 
 	@Override
